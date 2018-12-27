fe8af212d03b85413a5171414c80a498019d0d82
anjsahu19 <anjsahu19@gmail.com>
2018-12-27 11:46:24 +0530

    angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { CustomerComponent } from './customers/customer/customer.component';
+import { CustomerListComponent } from './customers/customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    CustomerComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule,
