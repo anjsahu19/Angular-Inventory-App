edc7d881c761ae189887d7d28177db18f860c7ef
anjsahu19 <anjsahu19@gmail.com>
2018-12-28 18:43:30 +0530

    Controller changes

--- InventoryApp/Controllers/CustomersController.cs
+++ InventoryApp/Controllers/CustomersController.cs
@@ -1,4 +1,4 @@
-﻿using Dapper;
+using Dapper;
 using InventoryApp.Models;
 using System;
 using System.Collections.Generic;
@@ -12,39 +12,52 @@ using System.Web.Http;
 
 namespace InventoryApp.Controllers
 {
-    public class CustomersController : ApiController
+  public class CustomersController : ApiController
+  {
+
+    // GET api/<controller>
+    public List<Customer> Get()
+    {
+      List<Customer> customers = new List<Customer>();
+      using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["InventoryApp"].ConnectionString))
+      {
+        customers = db.Query<Customer>("select * from Customer").ToList();
+      }
+      return customers;
+    }
+
+    // GET api/<controller>/5
+    public Customer Get(int id)
+    {
+      Customer customer = new Customer();
+      using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["InventoryApp"].ConnectionString))
+      {
+        customer = db.Query<Customer>(string.Format("select * from Customer Where Id={0}", id), new { id }).SingleOrDefault();
+      }
+
+      return customer;
+    }
+
+    // POST api/<controller>
+    public void Post([FromBody]Customer customer)
+    {
+      using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["InventoryApp"].ConnectionString))
+      {
+        string InsertQuery = "";
+        //customer = db.Execute()
+      }
+
+    }
+
+    // PUT api/<controller>/5
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+
+    // DELETE api/<controller>/5
+    public void Delete(int id)
     {
 
-        // GET api/<controller>
-        public List<Customer> Get()
-        {
-            List<Customer> customers = new List<Customer>();
-            using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["InventoryApp"].ConnectionString))
-            {
-                customers = db.Query<Customer>("select * from Customer").ToList();
-            }
-            return customers;
-        }
-
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
-        }
     }
+  }
 }
