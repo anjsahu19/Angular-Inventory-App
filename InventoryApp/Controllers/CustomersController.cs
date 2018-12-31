b743e7a47f003465a4470dc4b3cfed1fc517e42c
anjsahu19 <anjsahu19@gmail.com>
2018-12-31 17:05:18 +0530

    Customer Api updated methods

--- InventoryApp/Controllers/CustomersController.cs
+++ InventoryApp/Controllers/CustomersController.cs
@@ -8,6 +8,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 
 namespace InventoryApp.Controllers
@@ -39,25 +40,67 @@ namespace InventoryApp.Controllers
     }
 
     // POST api/<controller>
-    public void Post([FromBody]Customer customer)
+    public IHttpActionResult Post(Customer customer)
     {
+      int rowsInserted = 0;
       using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["InventoryApp"].ConnectionString))
       {
-        string InsertQuery = "";
-        //customer = db.Execute()
+        string InsertQuery = String.Format("Insert into Customer values({0},{1},{2},{3},{4})",
+          customer.FirstName,
+          customer.LastName,
+          customer.City,
+          customer.Country,
+          customer.Phone);
+
+        rowsInserted = db.Execute(InsertQuery);
       }
 
+      if (rowsInserted == 0)
+        return NotFound();
+      else
+        return Ok("New Customer Inserted");
     }
 
     // PUT api/<controller>/5
-    public void Put(int id, [FromBody]string value)
+    public IHttpActionResult Put(int id, Customer customer)
     {
+      int rowsUpdated = 0;
+      using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["InventoryApp"].ConnectionString))
+      {
+        string updateQuery = string.Format("UPDATE Customer SET FirstName={0},LastName={1},City={2},Country={3},Phone={4} where Id={5}",
+          customer.FirstName,
+          customer.LastName,
+          customer.City,
+          customer.Country,
+          customer.Phone,
+          id);
+
+        rowsUpdated = db.Execute(updateQuery);
+
+        if (rowsUpdated == 0)
+          return NotFound();
+        else
+          return Ok("Customer Details Updated!");
+
+      }
     }
 
     // DELETE api/<controller>/5
-    public void Delete(int id)
+    public IHttpActionResult Delete(int id)
     {
+      int rowsDeleted = 0;
+      using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["InventoryApp"].ConnectionString))
+      {
+        string DeleteQuery = string.Format("Delete from Customer where Id={0}", id);
+        rowsDeleted = db.Execute(DeleteQuery, new { Id = id });
+      }
+      if (rowsDeleted > 0)
+        return NotFound();
 
+      else
+        return Ok(string.Format("Customer With Id {0} deleted..", id));
     }
+
+
   }
 }
