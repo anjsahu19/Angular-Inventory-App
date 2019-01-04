b4fc418131b390decc28f74c6529b1935bebdaed
anjsahu19 <anjsahu19@gmail.com>
2019-01-04 18:58:55 +0530

    Working Edit Customer Module

--- src/app/customers/shared/customer.service.ts
+++ src/app/customers/shared/customer.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions, RequestMethod } from '@angular/http';
- import { Observable } from 'rxjs';
-
+import { Observable } from 'rxjs';
+ 
 import {Customer} from './customer.model'
 
 @Injectable({
@@ -32,12 +32,9 @@ export class CustomerService {
     debugger;
     var body = JSON.stringify(customer);
     var headerOptions = new Headers({'Content-Type':'application/json'});
-    var requestOptions = new RequestOptions({method : RequestMethod.Post,headers : headerOptions});
-      this.http.put('http://localhost:8089/api/Customers/Put/',body,requestOptions).toPromise().then(res=>{
-        debugger;
-        var result=res.text();
-        console.log(result);
-      })
+    var requestOptions = new RequestOptions({method : RequestMethod.Put,headers : headerOptions});
+      return this.http.put('http://localhost:8089/api/Customers/'+customer.Id,body,requestOptions).toPromise();
+     
   }
 
   deleteCustomer(Cid : number){
